2b7b2fa504bfa46e2379be96feddeb898ca209d2
Narendra Sompalli <141741512+Narendra9102@users.noreply.github.com>
2024-02-12 19:02:59 +0530

    Add files via upload

---
+++ app_django/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h2>Welcome to the Home Page!</h2>
+    </div>
+  )
+};
