3bd17d7cb26c7227ad75096839757607e2c3e340
Narendra Sompalli <141741512+Narendra9102@users.noreply.github.com>
2024-04-16 14:03:15 +0530

    Delete app_django/src directory

--- app_django/src/BuySell/BuyerAdd.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BuyerAdd() {
-  return (
-    <div>BuyerAdd</div>
-  )
-}
--- app_django/src/BuySell/SellerAdd.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SellerAdd() {
-  return (
-    <div>SellerAdd</div>
-  )
-}
