cf6a1547739379469cfdcd10531d3366338e3ae3
Narendra Sompalli <141741512+Narendra9102@users.noreply.github.com>
2024-06-24 11:28:34 +0530

    Update serializers.py

--- app_django/app/serializers.py
+++ app_django/app/serializers.py
@@ -1,12 +1,12 @@
-
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from .models import Customer , Product , Buyer , Seller , Fertilizer
+from .models import Customer, Product, Buyer, Seller, Fertilizer, Pros, Cart, CartItem
+from django.contrib.contenttypes.models import ContentType
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['customerName', 'mobileNumber', 'emailId', 'password']
+        fields = ['customerName', 'mobileNumber', 'emailId', 'password','user_id']
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -27,18 +27,55 @@ class LoginSerializer(serializers.Serializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name','image','brand','category','description','rating','price','_id']
+        fields = ['name', 'image', 'brand', 'category', 'description', 'rating', 'price', 'p_id']
+
 class FertilizerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fertilizer
-        fields = ['name','image','brand','category','description','rating','price','_id']
+        fields = ['name', 'image', 'brand', 'category', 'description', 'rating', 'price', 'f_id']
 
 class BuyerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Buyer
-        fields = ['name','image','phno','email','address','category','price','_id']
+        fields = ['name', 'image', 'phno', 'email', 'address', 'category', 'price', 'b_id']
+
+class ProsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pros
+        fields = ['category', 'price']
 
 class SellerSerializer(serializers.ModelSerializer):
+    products = ProsSerializer(many=True, required=False)
+
     class Meta:
         model = Seller
-        fields = ['name','image','phno','email','address','category','price','_id']
+        fields = ['name', 'image', 'phno', 'email', 'address', 'products', 's_id']
+
+    def create(self, validated_data):
+        products_data = validated_data.pop('products', [])
+        seller = Seller.objects.create(**validated_data)
+        for product_data in products_data:
+            Pros.objects.create(seller=seller, **product_data)
+        return seller
+
+class CartItemSerializer(serializers.ModelSerializer):
+    content_type = serializers.SlugRelatedField(queryset=ContentType.objects.all(), slug_field='model')
+    content_object = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ['content_type', 'object_id', 'content_object', 'quantity']
+
+    def get_content_object(self, obj):
+        if obj.content_type.model == 'product':
+            return ProductSerializer(obj.content_object).data
+        elif obj.content_type.model == 'fertilizer':
+            return FertilizerSerializer(obj.content_object).data
+        return None
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemSerializer(many=True, source='cartitem_set', read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['customer', 'cart_items']
